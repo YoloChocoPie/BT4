76be69fc664ad06e2d17d9447ce85b23a70fa41b
yolochocopie <chocopieyolo@gmail.com>
2019-10-01 14:45:04 +0700

    update#2 1-10-2019

---
+++ ConsoleApplication1/TestCeil/TestCeil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace TestCeil
+{
+    [TestClass]
+    public class TestCeil
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int result1 = Program.TestCeil((float)2.3);
+            Assert.AreEqual(3, result1);
+
+            int result2 = Program.TestCeil((float)-2.3);
+            Assert.AreEqual(-2, result2);
+
+            int result3 = Program.TestCeil((float)4);
+            Assert.AreEqual(4, result3);
+        }
+    }
+}
---
+++ ConsoleApplication1/TestFloat/TestFloor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace TestFloor
+{
+    [TestClass]
+    public class TestFloor
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int result1 = Program.TestFloor((float)2.3);
+            Assert.AreEqual(2, result1);
+
+            int result2 = Program.TestFloor((float)-2.3);
+            Assert.AreEqual(-2, result2);
+
+            int result3 = Program.TestFloor((float)3);
+            Assert.AreEqual(3, result3);
+        }
+    }
+}
